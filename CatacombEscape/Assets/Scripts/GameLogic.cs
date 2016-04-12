a3b1f8745b204bf273e86463065dc091ffcdbd32
Claire Rochelmeyer <7671148@student.swin.edu.au>
2016-04-12 13:53:50 +1000

    starting fresh

--- CatacombEscape/Assets/Scripts/GameLogic.cs
+++ CatacombEscape/Assets/Scripts/GameLogic.cs
@@ -3,13 +3,6 @@ using System.Collections;
 
 public class GameLogic : MonoBehaviour {
 
-
-    public void TestPassing(string pImageId, float px, float py)
-    {
-        Debug.Log("image id " + pImageId + " x y " + px + " " + py);
-    }
-
-
 	// Use this for initialization
 	void Start () {
 	
