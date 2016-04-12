f6c69e3e4afccf55d099f2c1df9446df971f1c2c
Hissatsu <Hissatsu93@hotmail.com>
2016-04-13 07:02:06 +1000

    hand generation and dragging completed for a rough demo 13/04 (GenerateHand() )...design/approach should be final though just not polished.

--- CatacombEscape/Assets/Scripts/GameLogic.cs
+++ CatacombEscape/Assets/Scripts/GameLogic.cs
@@ -25,7 +25,9 @@ public class GameLogic : MonoBehaviour
     {
         //approaching hand generation via grabbing each individual UI element and updating the sprite image and render...didnt work out 13/04
         //actaullyworking just rendered tiny and behind default image too...13/04
-        handTiles = GameObject.FindGameObjectsWithTag("handDrag");
+        handTiles = GameObject.FindGameObjectsWithTag("handDefault");
+        Debug.Log("handtiles length : " + handTiles.Length);
+        BtmPanel = GameObject.FindGameObjectWithTag("bottomPanel");
          //check for null
          if (handTiles != null)
          {
@@ -33,8 +35,26 @@ public class GameLogic : MonoBehaviour
              {
                 Debug.Log(handTiles[i]);
                 //set background image as sprite using spriterenderer had sizing/scale issues...
-                 handTiles[i].GetComponent<Image>().sprite = tileSprite[Random.Range(0, tileSprite.Length)] as Sprite;
-                 Debug.Log("trying to find component" + handTiles[i].GetComponent<Image>());
+                // handTiles[i].GetComponent<Image>().sprite = tileSprite[Random.Range(0, tileSprite.Length)] as Sprite;
+                //clone object 
+                //cloning object successful below but doesnt display properly and off propotions.
+                //GameObject newObject = Instantiate(handTiles[i],handTiles[i].transform.position,handTiles[i].transform.localRotation) as GameObject;
+                GameObject newObject = Instantiate(handTiles[i]);
+                newObject.transform.localScale = handTiles[i].transform.localScale;
+                newObject.transform.localPosition = handTiles[i].transform.localPosition;
+                //set tag so handTiles above doesnt grab clones as well.
+                newObject.tag = "handDrag";
+                //assign new object correct parents
+                newObject.transform.SetParent(BtmPanel.transform, false);
+                //use handdefaults to instantiate objects with rng sprite below and add script....
+                newObject.GetComponent<Image>().sprite = tileSprite[Random.Range(0, tileSprite.Length)] as Sprite;
+                newObject.AddComponent<Draggable>();
+                //above method with bool set to false solved instantiating flipped object....   
+                //newObject.transform.parent = handTiles[i].transform.parent;
+                
+                Debug.Log(handTiles[i].transform.parent + "   ");
+                //test[i] = GameObject.Instantiate(handTiles[i], handTiles[i].transform.position, Quaternion.identity);
+                Debug.Log("trying to find component" + handTiles[i] + " :: " + i + " :::" + handTiles.Length + "l");
                  Debug.Log("tilesprite[] " + tileSprite[Random.Range(0, tileSprite.Length)]);
              }
          }
@@ -86,11 +106,11 @@ public class GameLogic : MonoBehaviour
     void Update()
     {
         //do this every second
-        if (Time.time >= nextTime)
+        /*if (Time.time >= nextTime)
         {
             Debug.Log("mouse x: " + Input.mousePosition.x + " mouse y: " + Input.mousePosition.y);
             nextTime += interval;
-        }
+        }*/
 
     }
 }
