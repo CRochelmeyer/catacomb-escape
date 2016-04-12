91c0b79e0a7659164df1b2a6fd69d0565ec819d0
Hissatsu <Hissatsu93@hotmail.com>
2016-04-12 11:49:11 +1000

    Added temporary base scripts, completed basic drag drop for hand tiles

---
+++ CatacombEscape/Assets/Scripts/Draggable.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+    //use to handle when the tile is drag in an invalid spot return to position...
+    //not used at the moment 12/04
+    //public Transform parentToReturn = null;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnbeginDrag");
+        //save the parent incase of returns
+       // parentToReturn = this.transform.parent;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("onDrag");
+        this.transform.position = eventData.position;
+    }
+
+	public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        //this.transform.SetParent(parentToReturn);
+        //send mouse position and string of the sprite name to logic
+    }
+}
---
+++ CatacombEscape/Assets/Scripts/HandBtn.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandBtn : MonoBehaviour {
+    public GameObject[] handTiles;
+    public GameObject[] _hand;
+    public Canvas ui;
+
+    public void getHand()
+    {
+        Debug.Log("onClick");
+      /*  _hand = GameObject.FindGameObjectsWithTag("handTile");
+        Debug.Log("found with tag");
+        RectTransform panel = GameObject.Find("BottomPanel");
+        if (_hand.Length != 0)
+        {
+            for (int i = 0; i < _hand.Length; i++)
+            {
+
+                GameObject handChoice = handTiles[Random.Range(0, handTiles.Length)];
+                Transform target = _hand[i].transform;
+                Debug.Log(_hand[i].transform.parent);
+                Debug.Log("target pos " + target.position);
+                target.transform.SetParent(_hand[i].transform.parent);
+                var test = Instantiate(handChoice, target.position, Quaternion.identity);
+                Destroy(_hand[i]);
+                Debug.Log("for loop" + i);
+            }
+        }   
+        else { Debug.Log("_hand is empty"); }*/
+    }
+
+}
