1231840c2a06384511cc1d4ca03df65a7a94c0ac
Hissatsu <Hissatsu93@hotmail.com>
2016-05-26 17:42:40 +1000

    debug log of panels

--- CatacombEscape/Assets/Scripts/Panel.cs
+++ CatacombEscape/Assets/Scripts/Panel.cs
@@ -10,15 +10,27 @@ public class Panel : MonoBehaviour//, IPointerEnterHandler
 		GameLogic gameLogic = GameObject.FindObjectOfType<GameLogic> ();
 		Vector2 mousePosition = Input.mousePosition;
 		Vector3[] worldCorners = new Vector3[4];
+        Matrix4x4 test = new Matrix4x4();
 
         //grabs the grid panels rectransform component
 		RectTransform panel = this.gameObject.GetComponent<RectTransform>();
 
         //grabs the world corner of the individual grid panel into world corners
-		panel.GetWorldCorners(worldCorners);
+		panel.GetLocalCorners(worldCorners);
 
-		//check mouse coord within the 4 corner of the gridpanel
-		if (mousePosition.x >= worldCorners [0].x && mousePosition.x < worldCorners [2].x 
+        Debug.Log(panel.name);
+        Debug.Log("wc0 :: " + worldCorners[0]);
+        Debug.Log("wc1 :: " + worldCorners[1]);
+        Debug.Log("wc2 :: " + worldCorners[2]);
+        Debug.Log("wc3 :: " + worldCorners[3]);
+        this.gameObject.GetComponent<Panel>().GetComponent<RectTransform>().GetWorldCorners(worldCorners);
+        Debug.Log(panel.name + " :::::::");
+        Debug.Log("wc0 :: " + worldCorners[0]);
+        Debug.Log("wc1 :: " + worldCorners[1]);
+        Debug.Log("wc2 :: " + worldCorners[2]);
+        Debug.Log("wc3 :: " + worldCorners[3]);
+        //check mouse coord within the 4 corner of the gridpanel
+        if (mousePosition.x >= worldCorners [0].x && mousePosition.x < worldCorners [2].x 
 			&& mousePosition.y >= worldCorners [0].y && mousePosition.y < worldCorners [2].y)
 		{
 			gameLogic.MouseLocation = panel.name;
