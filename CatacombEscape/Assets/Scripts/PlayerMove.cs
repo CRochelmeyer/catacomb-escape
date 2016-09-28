8b2dfbf075d731ee8abbcc15ca6790da1e2f6525
Hissatsu <Hissatsu93@hotmail.com>
2016-09-28 11:04:33 +1000

    not working

--- CatacombEscape/Assets/Scripts/PlayerMove.cs
+++ CatacombEscape/Assets/Scripts/PlayerMove.cs
@@ -15,6 +15,7 @@ public class PlayerMove : MonoBehaviour
 	private string pDirection = "";
 	Vector3 targetPosition;
 	Vector3 initialPosition;
+    bool crRunning = false;
 
 	public float speed = 0.01f;
 	private float startTime;
@@ -40,6 +41,7 @@ public class PlayerMove : MonoBehaviour
 	public void FixedUpdate()
 	{
 		if (moving) {
+            //Debug.Log("if moving");
 			GameLogic gameLogic = GameObject.FindObjectOfType<GameLogic> ();
 			float distCovered = (Time.time - startTime) * speed;
 			float fracJourney = distCovered / journeyLength;
@@ -62,6 +64,7 @@ public class PlayerMove : MonoBehaviour
 					animator.SetInteger ("Direction", 2); //2=right
 					break;
 				}
+                //Debug.Log("InitPos" + initialPosition + " Destination :" + targetPosition);
 				player.transform.localPosition = Vector3.Lerp (initialPosition, targetPosition, fracJourney);
 
 				if (player.transform.localPosition == targetPosition) {
@@ -97,22 +100,28 @@ public class PlayerMove : MonoBehaviour
     //overload to handle pathfinder multiple movements
     public void UpdatePlayer(GameObject[] panel, List<string> path)
     {
-        //for each string in path -1
-        for (int i = 0; i<path.Count -1; i++)
+        //starting the path string 
+        for (int i = 1; i < path.Count; i++)
         {
-            pDirection = moveDir.MoveDirection(path[i], path[i+1]);
-            Debug.Log("pDir : " + pDirection);
-            int tempIndex;
-            cellindex.TryGetValue(path[i + 1], out tempIndex);
-            Vector3 tempPos = panel[tempIndex].transform.localPosition;
-            tempPos = new Vector3(tempPos.x, tempPos.y - playerDisp, tempPos.z);
-            targetPosition = tempPos;
-            initialPosition = player.transform.localPosition;
-            journeyLength = Vector3.Distance(initialPosition, targetPosition);
-            startTime = Time.time;
-            moving = true;
-            //update logic
+            while (crRunning == false)
+            {
+                int index;
+                cellindex.TryGetValue(path[i], out index);
+                Debug.Log("i = " + i + " " + path[i] + " :: ");
+                pDirection = moveDir.MoveDirection(path[i - 1], path[i]);
+                StartCoroutine("UpdatePlayerCoroutine", panel[index]);
+            }
         }
+    }
+
+    IEnumerator UpdatePlayerCoroutine(GameObject target)
+    {
+        Debug.Log("Start Coroutine ");
+        crRunning = true;
+        this.UpdatePlayer(target, pDirection);
+        yield return new WaitForSeconds(0f);
+        Debug.Log("after yield set crRunning");
+
     }
     public bool isMoving()
     {
