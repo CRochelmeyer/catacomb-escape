3af36a3ab330dcc8c878a50d80499552c3d416eb
Nick <9992057@student.swin.edu.au>
2016-10-24 18:27:47 +1100

    Reset a tile's ID if it was 'event_red' once the enemy has moved.

--- CatacombEscape/Assets/Scripts/Tile.cs
+++ CatacombEscape/Assets/Scripts/Tile.cs
@@ -86,6 +86,10 @@ public class Tile
     }
     public void ClearEvent()
     {
+
+		if (_tileID == "event_red"){
+			_tileID = "";
+		}
         _event = "";
         _eventItem = "";
 		_nextMove = "";
